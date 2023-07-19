7b7d1927f63bf1a0dcc61820182ab46703c4009b
AissaSemaoui <semaoui0347@gmail.com>
2023-07-19 16:27:04 +0100

    fix

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,12 +2,9 @@ import mongoose from "mongoose";
 
 export const connectToDatabase = async () => {
   mongoose
-    .connect(
-      "mongodb+srv://semaoui0347:Hdn3GMsHfvkQxwVw@nasacluster.hfmxup0.mongodb.net/",
-      {
-        dbName: "carwash-app",
-      }
-    )
+    .connect(process.env.MONGO_URI, {
+      dbName: "carwash-app",
+    })
     .then(({ connection }) =>
       console.log(`Database Connected on ${connection.host}`)
     )
